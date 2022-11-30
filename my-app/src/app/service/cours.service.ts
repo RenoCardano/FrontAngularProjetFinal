17f124515414c60510067f244c750a8b5e69456e
MehdiBenlamine <BenlamineMehdi@gmail.com>
2022-11-30 08:18:18 +0100

    Ajout du service cours pour get et post les cours dans la BDD

---
+++ my-app/src/app/service/cours.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+import {Cours} from "../modele/cours.modele";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursService {
+
+  url= 'http://localhost:8081/api/cours'
+
+  constructor(private http: HttpClient) { }
+
+  findAll():Observable<Cours[]>{
+    return this.http.get<Cours[]>(this.url);
+  }
+
+  add(cours: Cours) {
+    const headers = new HttpHeaders({'Content-Type':'application/json'});
+    let body = JSON.stringify(cours);
+    this.http.post<Cours>(this.url, body, {headers: headers}).subscribe();
+  }
+}
